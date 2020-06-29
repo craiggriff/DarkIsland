dff622d059fa4f719b7c694ca07f687adb5ee088
Craig Griffiths <craigsaint@outlook.com>
2020-06-29 06:27:06 +0100

    compiled items included

---
+++ DarkIsland/Bullet/out/build/x64-Debug (default)/src/cmake_install.cmake
@@ -0,0 +1,45 @@
+# Install script for directory: D:/Desktop/DarkIsland/DarkIsland/Bullet/src
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "d:/Desktop/DarkIsland/DarkIsland/Bullet/out/install/x64-Debug (default)")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("D:/Desktop/DarkIsland/DarkIsland/Bullet/out/build/x64-Debug (default)/src/BulletSoftBody/cmake_install.cmake")
+  include("D:/Desktop/DarkIsland/DarkIsland/Bullet/out/build/x64-Debug (default)/src/BulletCollision/cmake_install.cmake")
+  include("D:/Desktop/DarkIsland/DarkIsland/Bullet/out/build/x64-Debug (default)/src/BulletDynamics/cmake_install.cmake")
+  include("D:/Desktop/DarkIsland/DarkIsland/Bullet/out/build/x64-Debug (default)/src/LinearMath/cmake_install.cmake")
+  include("D:/Desktop/DarkIsland/DarkIsland/Bullet/out/build/x64-Debug (default)/src/MiniCL/cmake_install.cmake")
+  include("D:/Desktop/DarkIsland/DarkIsland/Bullet/out/build/x64-Debug (default)/src/BulletMultiThreaded/cmake_install.cmake")
+
+endif()
+
